fd5bf50f0edb956a78d5490a52ffffdeb63a2323
Bhushanab159 <106249755+Bhushanab159@users.noreply.github.com>
2022-12-18 19:36:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import "./../styles/App.css";
 
 function App() 
 {
-  const [displayMessage, setDisplayMessage] = useState(false);
+  const [display, setDisplay] = useState(false);
 
   return (
 	<div id="main">
 		{
     		(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
     	}
-		<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
+		<button id="click" onClick={()=>setDisplay(true)}>Click me</button>
     </div>
   );
 }
